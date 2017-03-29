9ffd8cd0ce945b9210730b11cec72eebe0e55b04
Philippe Courtemanche <philippecourtemanche@Sansfil-Securise-Etudiants-Lassonde-244-103.polymtl.ca>
2017-03-28 20:20:08 -0400

    formatage du json qui vient du url

--- LOG2420/TD/TD3/main.js
+++ LOG2420/TD/TD3/main.js
@@ -1,8 +1,62 @@
-  var map;
-function initMap() {
-map = new google.maps.Map(document.getElementById('map'), {
-  
-  center: {lat: -34.397, lng: 150.644},
-  zoom: 8
+var map;
+var listStations = {};
+
+$(document).ready(function () {
+  // ...
+  // ...
+  initMap();
+  loadData();
+
+
 });
+
+
+
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8
+  });
+}
+
+function loadData() {
+  $.get('https://secure.bixi.com/data/stations.json')
+    .done(function (data) {
+      data.stations.forEach(function (element) {
+        var station = createStation(element);
+        listStations[station.nom] = station;
+
+
+      }, this);
+
+        //creer un tableau pour l auto completion et etre capable de recuperer l objet a partir du nom
+
+
+      console.log(Object.keys(listStations));
+
+
+      // ...
+      // ...
+      //console.log(data.stations);
+
+
+    })
+
+
+}
+
+function createStation(infoStation) {
+  return station = {
+    'id': infoStation.id,
+    'nom': infoStation.s,
+    'statut':infoStation.b,
+    'etat':infoStation.su,
+    'actif':infoStation.m,
+    'VDispo':infoStation.ba,
+    'BDispo':infoStation.da,
+    'VNDispo':infoStation.bx,
+    'BNDispo':infoStation.dx,
+  }
 }
