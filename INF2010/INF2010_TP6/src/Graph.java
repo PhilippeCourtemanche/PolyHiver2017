9eaba25ecad92e3c6732bcbd319c027f96917b36
Marc HOUNTO <Marc HOUNTO>
2017-04-07 07:19:47 -0400

    graphe presque fini reste encore un peu de bugs a corriger mais fonctionne

--- INF2010/INF2010_TP6/src/Graph.java
+++ INF2010/INF2010_TP6/src/Graph.java
@@ -1,35 +1,87 @@
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 
 public class Graph {
 
-	private List<Node> nodes; // Noeuds
-	private List<Edge> edges; // Les arcs
+	private List<Node> nodes = new ArrayList<Node>(); // Noeuds
+	private List<Edge> edges= new ArrayList<Edge>(); // Les arcs
 	static final double inf = 99999;
 	public Graph() {
-		
+
 	}
-	
+
 	//permet de creer le graphe a partir d'un fichier "graphte.txt"
 	public void readFromFile(String filePath,String separtor){
-		// A completer
-		
+		// A completer 
+
+		try {
+			File file = new File("./src/graphe.txt");
+			Scanner scanner = new Scanner(file);
+			String[] strArr = scanner.nextLine().split(separtor);
+			int id=0;
+			for(String str:strArr){
+				Node n =new Node(id,str);
+				//List<Node> nodes = new ArrayList<Node>();
+				nodes.add(n);
+				id++;
+				// System.out.println(n.getName());
+			}
+
+
+			while (scanner.hasNextLine()) {
+
+				for(Node n1:nodes){
+					int indice=0;
+					double distance = 0;
+					String[] strArrD = scanner.nextLine().split(separtor);
+					for(Node n2:nodes ){
+						try{
+						
+							 distance=Double.parseDouble(strArrD[indice]);
+						}
+						catch(NumberFormatException f)
+						{
+							distance=0;
+						}
+						
+							
+						Edge e =new Edge(n1,n2,distance);
+						//List<Edge> edges = new ArrayList<Edge>();
+						edges.add(e);
+						indice++;
+						System.out.println(e.getSource().getName() + " " + e.getDestination().getName() + " " + e.getDistance());
+					}
+				}
+			}
+
+			scanner.close();
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+
+		}
+
 		//avec la premiere ligne on cree les noeuds
 		//on boucle a travers les lignes restantes
-			//pour le noeud courant on sauvegarde la distance entre ce noeud courant et les autres noeuds 
+		//pour le noeud courant on sauvegarde la distance entre ce noeud courant et les autres noeuds 
 	}
-	
+
 	//permet de retourner tous les arcs sortant d'un noeud source
 	public List<Edge> getOutEdges(Node source) {
 		List<Edge> outEdges = new LinkedList<Edge>(); 
 		// A completer
-		
+
 		//parcourir la liste d'arcs
 		for(Edge arc : edges)
 		{
@@ -39,23 +91,23 @@ public class Graph {
 				outEdges.add(arc);
 			}
 		}
-			
+
 		return outEdges;	
 	}
-	
+
 	public List<Edge> getInEdges(Node dest) {
 		List<Edge> inEdges = new LinkedList<Edge>(); 
 		// A completer 
-		
+
 		//parcourir la liste d'arcs
-				for(Edge arc : edges)
-				{
-					//si un arc est entrant au noeud source, on l'ajoute a la liste d'arcs entrants
-					if(arc.getSource() == dest)
-					{
-						inEdges.add(arc);
-					}
-				}
+		for(Edge arc : edges)
+		{
+			//si un arc est entrant au noeud source, on l'ajoute a la liste d'arcs entrants
+			if(arc.getSource() == dest)
+			{
+				inEdges.add(arc);
+			}
+		}
 		return inEdges;		
 	}
 	// Accesseurs 
--- INF2010/INF2010_TP6/src/Main.java
+++ INF2010/INF2010_TP6/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 	public static void main(String[] args) {
 		// Exerice 1: creation de graphe à partir du fichier 
 		Graph g = new Graph();
-		g.readFromFile("./src/graphe.txt",",");
+		g.readFromFile("./src/graphe.txt","\\,");
 		// Exerice 2: calcul du plus court chemin de source vers tous les sommet
 		Bellman bl = new Bellman(g);
 		Node source=g.getNodeByName("S");
